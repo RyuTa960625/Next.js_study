3befcfc096b7a8f7a49c089672053667986576d8
류태수 <r.tae.su.0625@gmail.com>
2024-09-23 23:37:01 +0900

    app router

--- onebite-book-app-router/next.config.mjs
+++ onebite-book-app-router/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    domains: ["shopping-phinf.pstatic.net"],
+  },
+};
 
 export default nextConfig;
